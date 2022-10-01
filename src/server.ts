2824bdc9a39d9adb8ffa3c6e3e4e628ba7f75eac
davy <davydesouzabar@gmail.com>
2022-09-30 23:38:08 -0400

    feat: use app routes

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import { categoriesRoutes } from "./routes"
 const app = express()
 app.use(express.json())
+
+app.use("/categories",categoriesRoutes)
 app.listen(3333)
