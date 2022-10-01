e1e6359cfda2278d908d820716feae2d50cf031b
davy <davydesouzabar@gmail.com>
2022-10-01 15:52:59 -0400

    refactor: rename file

--- src/modules/cars/services/Category/CreateCategoryService.ts
+++ src/modules/cars/services/Category/CreateCategoryService.ts
@@ -1,4 +1,4 @@
-import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+import { ICategoriesRepository } from "../../repositories/Category/ICategoriesRepository";
 
 interface IRequest {
   name: string;
