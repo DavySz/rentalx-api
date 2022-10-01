e57ab3d34fcaa27de5d13eebfb2de3b521473ca8
davy <davydesouzabar@gmail.com>
2022-09-30 23:37:58 -0400

    feat: create findByName method

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,36 @@
+import { Category } from "../model/category";
+
+interface ICreateCategoryDTO {
+    name: string;
+    description: string;
+}
+
+export class CategoriesRepository {
+    constructor(){
+        this.categories = []
+    }
+ 
+    private categories: Category[];
+
+   create({description,name}:ICreateCategoryDTO): void {
+    const category = new Category()
+
+    Object.assign(category, {
+        name,
+        description,
+        created_at: new Date()
+    })
+
+    this.categories.push(category)
+   }
+
+   list(): Category[] {
+    return this.categories
+   }
+
+   findByName(name:string): Category {
+    const category = this.categories.find(category => category.name===name)
+    return category
+   }
+
+}
