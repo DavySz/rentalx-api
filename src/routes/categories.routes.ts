f42877ed49911ab92c6d719bc16f877a672d673f
davy <davydesouzabar@gmail.com>
2022-10-01 12:13:28 -0400

    fix: eslint

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -6,27 +6,27 @@ const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
+  const { name, description } = request.body;
 
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-    if (categoryAlreadyExists) {
-        return response.status(400).json({
-            error: "Category already exists!",
-        });
-    }
-
-    categoriesRepository.create({
-        name,
-        description,
+  if (categoryAlreadyExists) {
+    return response.status(400).json({
+      error: "Category already exists!",
     });
+  }
 
-    return response.status(201).send();
+  categoriesRepository.create({
+    name,
+    description,
+  });
+
+  return response.status(201).send();
 });
 
 categoriesRoutes.get("/list", (_request, response) => {
-    const categories = categoriesRepository.list();
-    return response.json(categories);
+  const categories = categoriesRepository.list();
+  return response.json(categories);
 });
 
 export { categoriesRoutes };
