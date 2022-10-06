28e3f37a859bfa9ddd02446b7ceaad7a33ed3e9b
davy <davydesouzabar@gmail.com>
2022-10-06 15:37:34 -0400

    refactor: using dependency injection

--- src/modules/cars/useCases/Category/importCategory/index.ts
+++
@@ -1,9 +0,0 @@
-import { CategoriesRepository } from "../../../repositories/Category/CategoriesRepository";
-import { ImportCategoryController } from "./ImportCategoryController";
-import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
-
-const importCategoryController = new ImportCategoryController(
-  new ImportCategoryUseCase(CategoriesRepository.getInstance())
-);
-
-export { importCategoryController };
--- src/modules/cars/useCases/Specification/listSpecification/index.ts
+++
@@ -1,9 +0,0 @@
-import { SpecificationsRepository } from "../../../repositories/Specification/SpecificationsRepository";
-import { ListSpecificationsController } from "./listSpecificationsController";
-import { ListSpecificationsUseCase } from "./listSpecificationsUseCase";
-
-const listSpecificationsController = new ListSpecificationsController(
-  new ListSpecificationsUseCase(SpecificationsRepository.getInstance())
-);
-
-export { listSpecificationsController };
