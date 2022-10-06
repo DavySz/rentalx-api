d1860d06df9550a277f8d6f76853d054f42cc201
davy <davydesouzabar@gmail.com>
2022-10-06 15:36:20 -0400

    feat: using container resolve

--- src/modules/cars/useCases/Specification/listSpecification/listSpecificationsController.ts
+++ src/modules/cars/useCases/Specification/listSpecification/listSpecificationsController.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ListSpecificationsUseCase } from "./listSpecificationsUseCase";
 
 export class ListSpecificationsController {
-  constructor(private listSpecificationsUseCase: ListSpecificationsUseCase) {}
-  handle(_request: Request, response: Response): Response {
-    const specifications = this.listSpecificationsUseCase.execute();
+  async handle(_request: Request, response: Response): Promise<Response> {
+    const listSpecificationsUseCase = container.resolve(
+      ListSpecificationsUseCase
+    );
+    const specifications = await listSpecificationsUseCase.execute();
     return response.json(specifications);
   }
 }
