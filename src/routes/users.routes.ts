e204f3b41bb08104a48d2d4ca58ac3639ee4fe92
davy <davydesouzabar@gmail.com>
2022-10-20 21:28:26 -0400

    feat: create update user avatar route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,25 @@
 import { Router } from "express";
+import multer from "multer";
 
-import { ensureAuthenticated } from "../middlewere/ensureAuthenticated";
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/User/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/User/updateUserAvatar/UpdateUserAvatarController";
+
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
-
-usersRoutes.use(ensureAuthenticated);
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
+
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes };
